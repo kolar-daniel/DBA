b63997e76399a70ca35f2393922505bf0ff01055
stemberk <stemberk@spsmb.cz>
2022-04-29 11:43:01 +0200

    Ukol

--- src/w32/sqlite/FUkol.java
+++ src/w32/sqlite/FUkol.java
@@ -1,5 +1,8 @@
 package w32.sqlite;
 // Převeďte data z https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19
+// (COVID-19: Přehled osob s prokázanou nákazou dle hlášení krajských
+// hygienických stanic (v2)).
+// K nalezení zde: Y:\stemberk\verejne_zaci\osoby.csv
 // do databáze (můžete si vybrat mezi SQLite, či MySQL) a zobrazte
 // v TableView, případně v nějakém grafu JavaFX.
 public class FUkol {
