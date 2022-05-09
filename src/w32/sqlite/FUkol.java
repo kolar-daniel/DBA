b203440ba7915875cc32c7b6822b78aafeb883a4
stemberk <stemberk@spsmb.cz>
2022-05-09 14:54:03 +0200

    Ukol

--- src/w32/sqlite/FUkol.java
+++ src/w32/sqlite/FUkol.java
@@ -7,10 +7,25 @@ package w32.sqlite;
 // Totéž proveďte pro soubor C:\stemberk\verejne_zaci\staty.csv a zobrazte
 // v TableView, případně v nějakém grafu JavaFX.
 
+import java.io.*;
+
 /**
  * Struktura tabulky:
  * id,datum,vek,pohlavi,kraj_nuts_kod,okres_lau_kod,nakaza_v_zahranici,nakaza_zeme_csu_kod,reportovano_khs
  * 1ea976a2-896a-40b2-b617-b780a713323d,2020-03-01,43,M,CZ042,CZ0421,1,IT,1
  */
 public class FUkol {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = null;
+        String radka;
+        int cnt = 0;
+        fr = new FileReader("Y:\\stemberk\\verejne_zaci\\osoby.csv");
+        BufferedReader br = new BufferedReader(fr);
+        while((radka = br.readLine()) != null) {
+
+            String[] hodnoty = radka.split(",");
+            System.out.format("%s -- %s %n", hodnoty[0], hodnoty[1]);
+            if (cnt++ > 10) break;
+        }
+    }
 }
